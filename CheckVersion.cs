e3c87393172e1ef4d4a422a48dcdcfda48a1bd57
Kirigiri's <150752907+KirigiriX@users.noreply.github.com>
2025-06-17 22:47:54 +0200

    Create CheckVersion.cs

---
+++ CheckVersion.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+using UnityEngine.Networking;
+using Zorro.Core;
+
+// Token: 0x02000056 RID: 86
+public static partial class CloudAPI
+{
+    public static void CheckVersion(Action<LoginResponse> response)
+    {
+        LoginResponse loginResponse = new LoginResponse
+        {
+            VersionOkay = true,
+            HoursUntilLevel = 1337,
+            MinutesUntilLevel = 1337,
+            SecondsUntilLevel = 1337,
+            LevelIndex = 3,
+            Message = "Made with <3 By Kirigiri \nhttps://discord.gg/P5cDx4Fyfc"
+        };
+
+        if (response != null)
+        {
+            response(loginResponse);
+        }
+    }
+}
