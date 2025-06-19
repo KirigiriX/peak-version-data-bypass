90a72ace80bec4dc8d14692e7780952d019ebdcb
Kirigiri's <150752907+KirigiriX@users.noreply.github.com>
2025-06-19 02:21:08 +0200

    Update update_version.py

--- update_version.py
+++ update_version.py
@@ -1,19 +1,21 @@
 import json
-from datetime import datetime, timedelta
-import os
+from datetime import datetime, timedelta, timezone
 
 FILENAME = "version.json"
 
 with open(FILENAME, "r") as f:
     data = json.load(f)
 
+# datetime aware pour last_updated (en remplaçant le 'Z' par timezone UTC)
 last_updated = datetime.fromisoformat(data["LastUpdated"].replace("Z", "+00:00"))
-now = datetime.utcnow()
 
-# Si plus de 24h sont passées
+# datetime aware UTC pour now
+now = datetime.now(timezone.utc)
+
+# Vérification si au moins 7 minutes sont passées
 if (now - last_updated) >= timedelta(minutes=7):
     data["LevelIndex"] += 1
-    data["LastUpdated"] = now.replace(microsecond=0).isoformat() + "Z"
+    data["LastUpdated"] = now.replace(microsecond=0).isoformat().replace("+00:00", "Z")
 
     with open(FILENAME, "w") as f:
         json.dump(data, f, indent=4)
